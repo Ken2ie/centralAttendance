ef4a96fe3c7d055339d7ceb353aa58de258b09c4
SalamiTech <salamigideon01@gmail.com>
2023-07-26 04:11:56 +0000

    iOS build

--- lib/main.dart
+++ lib/main.dart
@@ -6,24 +6,21 @@ import 'package:class_attendance_app/login/registerSecond.dart';
 import 'package:class_attendance_app/splashscreen.dart';
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(MaterialApp(
-    theme: ThemeData(
-   fontFamily: 'Poppins' 
-   
-),
+    theme: ThemeData(fontFamily: 'Poppins'),
     debugShowCheckedModeBanner: false,
     routes: {
       // SplashScreen
       "/": (context) => const Splash(),
       // "/intro": (context) => Intro(),
 
-      // Sign, Signup, Forgot Password 
+      // Sign, Signup, Forgot Password
       "/login": (context) => const Login(),
       "/register": (context) => const Register(),
       // "/registerS": (context) => const RegisterSecond(),
       "/forgot": (context) => const Forgot(),
-      
+
       // Dashboard
       "/navigator": (context) => const Navigator(),
       "/dashboard": (context) => const Dashboard(),
